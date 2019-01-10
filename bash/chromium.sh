e2ae131640aec79f67131469a325242b87a8cd31
Ian <ian.ansell.uk@gmail.com>
2019-01-10 12:22:45 +0000

    change tabs

--- bash/chromium.sh
+++ bash/chromium.sh
@@ -10,6 +10,3 @@ https://datastudio.google.com/reporting/1S712euH7S8dprfCUwrMQaUcxtIJKjAE5/page/Q
 https://datastudio.google.com/reporting/1cyiWdnfS0oZmhoKNnuOPMyzzwtL7CrBb/page/Q7XN \
 https://datastudio.google.com/reporting/1cyiWdnfS0oZmhoKNnuOPMyzzwtL7CrBb/page/j7XN \
 https://datastudio.google.com/reporting/1cyiWdnfS0oZmhoKNnuOPMyzzwtL7CrBb/page/F7bN \
-https://trello.com/b/K9JPbTSj/maintenance-backlog \
-https://trello.com/b/c4XKOv5K/content-in-play-used-to-be-maintenance-2 \
-https://datastudio.google.com/reporting/1hBHJhEVCu4f25m7UHca17uIUKDY5S_Ax/page/IPOO \
