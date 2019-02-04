9555ee2242db322f9f2a6d060a4f1487031c8b55
Ian <ian.ansell.uk@gmail.com>
2019-02-04 11:17:46 +0000

    change to use gtm api v1 rather than v2, due to the requirement to include workspace id

--- python/gtm.py
+++ python/gtm.py
@@ -1,17 +1,8 @@
-# Pulls data from Analytics API and uploads it to BigQuery.
-
-#TODO if no arguments are passed, then request an input from the user
-
 from apiclient.discovery import build
 from oauth2client.service_account import ServiceAccountCredentials #depreciated
-from google.oauth2 import service_account
 import httplib2
 import json
-import pandas as pd
-import pandas_gbq
-from pathlib import Path
 import os
-import sys
 import logging
 
 def get_report():
@@ -30,20 +21,24 @@ def get_report():
     http = httplib2.Http()
     http = credentials.authorize(http)
 
-    paths = {"Mouseflow pages - All":"accounts/1090055821/containers/6311504/workspaces/208/variables/57",
-             "Mouseflow pages - public":"accounts/1090055821/containers/6311504/workspaces/208/variables/21",
-             "Mouseflow pages - adviser":"accounts/1090055821/containers/6311504/workspaces/208/variables/56"}
+    vars = {"Mouseflow pages - All":"57",
+             "Mouseflow pages - public":"21",
+             "Mouseflow pages - adviser":"56"}
 
-    gtm_service = build('tagmanager', 'v2', http=http)
+    gtm_service = build('tagmanager', 'v1', http=http)
     sheets_service = build('sheets', 'v4', http=http)
     sheet = sheets_service.spreadsheets()
 
     sheet.values().clear(spreadsheetId=SPREADSHEET_ID,
                                 range=clear_RANGE_NAME).execute()
 
-    for path, value in paths.items():
-        response = gtm_service.accounts().containers().workspaces().variables().get(path=value).execute()
-        clean = json.dumps(response)
+    for var, value in vars.items():
+        variable = gtm_service.accounts().containers().variables().get(
+            accountId='1090055821',
+            containerId='6311504',
+            variableId=value
+        ).execute()
+        clean = json.dumps(variable)
         parsed_json = json.loads(clean)
         j_list = parsed_json['parameter'][6]['list']
 
@@ -53,7 +48,7 @@ def get_report():
             body = {"range": "data!A2:E",
                     "majorDimension": "ROWS",
                     "values": [
-                    [path, name, val],
+                    [var, name, val],
                     ],}
             sheet.values().append(spreadsheetId=SPREADSHEET_ID, range=clear_RANGE_NAME, body=body, valueInputOption='USER_ENTERED').execute()
 
